61062c167b2c34c757aaae479618b39e628a38a6
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-02-12 15:31:07 +0300

    Fix route base url

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import Calculate from './pages/Calculate';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="lucky-number" element={<LuckyNumber />} />
